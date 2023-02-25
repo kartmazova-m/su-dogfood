59bf39f9b38aeb4dd188c77ce42180e5ade901e3
unknown <kartmazova.m@mail.ru>
2023-02-25 22:26:18 +0300

    Test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+ const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <div className="header__wrapper">
+                    Лого сюда
+                </div>
+            </div>
+        </header>
+    )
+ };
+
+ export default Header;
